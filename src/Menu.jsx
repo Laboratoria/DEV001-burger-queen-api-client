9532f1f5e567bab66083058345a1445d18f3c4ea
Lorenaguillen <guillen.lorena88@gmail.com>
2023-02-02 09:31:02 -0500

    haciendo pruebas

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,16 +1,38 @@
 import Productos from "./components/Productos"
-import {useState} from 'react'
+import { useState, useEffect, useContext } from 'react';
+import ProductContext from './src/components/DataContext.jsx';
 
 function Menu () {
 
+    const { items } = useContext(ProductContext)
+    const [db, setDb] = useState([]);
+
+    useEffect(() => {
+        const products = async () => {
+            await fetch('http://localhost:3004/users') // hacemos la petición get
+                .then(res => res.json()) // cuando hayamos terminado (then) parseamos a json la respuesta de la petición
+                .then(res => setDb(res)); // cuando hayamos terminado (then) actualizamos el estado nombre
+        }
+        products()
+    }, []); 
     
     return (
         <>
             <>
                 <Productos
-                    optionSelect='Café Americano'
+                    producSelect='Café Americano'
                     cost='S/5.00'
-                    add='Agregar'
+                    //buttonClassName='Agregar'
+                />
+                <Productos
+                    producSelect='Café Americano'
+                    cost='S/5.00'
+                    //buttonClassName='Agregar'
+                />
+                <Productos
+                    producSelect='Café Americano'
+                    cost='S/5.00'
+                    //buttonClassName='Agregar'
                 />
             </>
         </>
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,9 +1,38 @@
+import { createContext, useContext, useState } from 'react';
+import ProductContext from './src/components/DataContext.jsx';
+
+const producSelect = createContext();
 
 function Productos({ producSelect, cost }) {
+    
+    const [isAdded, setIsAdded] = useState(false)
+    const { items } = useContext(ProductContext)
+    const { setItems } = useContext(ProductContext)
+
+    const addProduct = (product) => {
+        const productAdded = items.find(productAdded => productAdded.producSelect === product.producSelect)
+        if (!productAdded) {
+            setItems(items => [...items, product])
+            setIsAdded(true)
+        } else {
+            setItems(products => products.filter(product => product.producSelect !== productAdded.producSelect))
+            setIsAdded(false)
+        }
+    }
+
+
+    let text = isAdded ? 'Eliminar' : 'Añadir';
+    let buttonClassName = isAdded
+        ? 'add-products delete'
+        : 'add-products'
+    
     return (
         <div className="caja-opciones">
-            <p className="opcion">{producSelect}</p>
-            <p className="cost">{cost}</p>
+            <div className='caja-producto'>
+                <p className="opcion">{producSelect}</p>
+                <p className="cost">{cost}</p>
+                <button className={buttonClassName} onClick={() => addProduct({ cost, productName })}>{text}</button>
+            </div>
         </div>
     );
 }
