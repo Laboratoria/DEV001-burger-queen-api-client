73dd2851f5a05b0638ce5b3021e69d5baf310ed4
Lorenaguillen <guillen.lorena88@gmail.com>
2023-02-13 10:36:59 -0500

    quitando comas

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import { ProductContextProvider } from './components/DataContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,4 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </ProductContextProvider>
   </BrowserRouter>
-);
+)
