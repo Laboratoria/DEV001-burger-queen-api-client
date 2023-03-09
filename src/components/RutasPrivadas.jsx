0493272d8d7617d057a388c5b6b9c1202e545b0c
Adriana Araujo <araujovillasmil@gmail.com>
2023-03-09 11:15:06 -0300

    ruta admin

--- src/components/RutasPrivadas.jsx
+++ src/components/RutasPrivadas.jsx
@@ -1,15 +1,18 @@
 import { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuth } from './UseAuth'
-export function RutasPrivadas ({ children }) {
+export function RutasPrivadas ({ children, isAlowed }) {
   const { login } = useAuth()
-  const user = JSON.parse(window.localStorage.getItem('user'))
+  const user = JSON.parse(window.sessionStorage.getItem('user'))
   useEffect(() => {
-    login(user)
+    login()
   }, [])
 
   if (!user) {
     return <Navigate to='/' />
   }
-  return children
+  console.log(isAlowed)
+  return isAlowed ? children : 'no tienes permiso'
+
+  // && user.user.roles.mesero ? <Navigate to='/menu' /> : <Navigate to='/admin' />
 }
